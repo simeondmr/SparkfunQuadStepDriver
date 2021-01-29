94f879746b05b25aea0cd1f6b7f5b4014d69badc
simeondmr <dmrsim94@gmail.com>
2021-01-29 20:36:50 +0000

    Small code refactor

--- src/lib.rs
+++ src/lib.rs
@@ -48,20 +48,21 @@ pub enum StepSize {
 
 const STEPMIN: u32 = 800;
 
-const STEP_PIN: ((u8,u8, u8), (u8,u8, u8), (u8,u8, u8),(u8,u8, u8),(u8,u8, u8)) = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 0), (1, 1, 1));
+const STEP_MODE: ((u8,u8, u8), (u8,u8, u8), (u8,u8, u8),(u8,u8, u8),(u8,u8, u8)) = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 0), (1, 1, 1));
 
 impl SparkfunQuadStep {
     pub fn new(mot_stp1: u32, mot_stp2: u32, mot_stp3: u32, mot_stp4: u32) -> Result<SparkfunQuadStep, gpio_cdev::Error> {
-        let mot_stp1 = Chip::new("/dev/gpiochip0")?
+        let mut gpio_chip = Chip::new("/dev/gpiochip0")?;
+        let mot_stp1 = gpio_chip
             .get_line(mot_stp1)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp2 = Chip::new("/dev/gpiochip0")?
+        let mot_stp2 = gpio_chip
             .get_line(mot_stp2)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp3 = Chip::new("/dev/gpiochip0")?
+        let mot_stp3 = gpio_chip
             .get_line(mot_stp3)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp4 = Chip::new("/dev/gpiochip0")?
+        let mot_stp4 = gpio_chip
             .get_line(mot_stp4)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
         mot_stp1.set_value(0)?;
@@ -91,20 +92,20 @@ impl SparkfunQuadStep {
 
     //This method allow to add a new stepper
     pub fn add_mot(&mut self, mot: Motor, mot_en: u32, mot_dir: u32, mot_ms1: u32, mot_ms2: u32, mot_ms3: u32) -> Result<(), gpio_cdev::Error> {
-        println!("OKOOKOKOKO");
-        let mot_en = Chip::new("/dev/gpiochip0")?
+        let mut gpio_chip = Chip::new("/dev/gpiochip0")?;
+        let mot_en = gpio_chip
             .get_line(mot_en)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_dir = Chip::new("/dev/gpiochip0")?
+        let mot_dir = gpio_chip
             .get_line(mot_dir)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_ms1 = Chip::new("/dev/gpiochip0")?
+        let mot_ms1 = gpio_chip
             .get_line(mot_ms1)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_ms2 = Chip::new("/dev/gpiochip0")?
+        let mot_ms2 = gpio_chip
             .get_line(mot_ms2)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_ms3 = Chip::new("/dev/gpiochip0")?
+        let mot_ms3 = gpio_chip
             .get_line(mot_ms3)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
         mot_en.set_value(1)?;
@@ -179,20 +180,19 @@ impl SparkfunQuadStep {
             Motor::Ch4 => self.ch4.as_ref().unwrap()
         };
         let ms_pin = match step_size {
-            StepSize::One=> STEP_PIN.0,
-            StepSize::Two => STEP_PIN.1,
-            StepSize::Four => STEP_PIN.2,
-            StepSize::Eight => STEP_PIN.3,
-            StepSize::Sixteen => STEP_PIN.4
+            StepSize::One=> STEP_MODE.0,
+            StepSize::Two => STEP_MODE.1,
+            StepSize::Four => STEP_MODE.2,
+            StepSize::Eight => STEP_MODE.3,
+            StepSize::Sixteen => STEP_MODE.4
         };
-
         ch.mot_dir.as_ref().unwrap().set_value(dir)?;
         ch.mot_ms1.as_ref().unwrap().set_value(ms_pin.0)?;
         ch.mot_ms2.as_ref().unwrap().set_value(ms_pin.1)?;
         ch.mot_ms3.as_ref().unwrap().set_value(ms_pin.2)?;
         ch.mot_en.as_ref().unwrap().set_value(0)?;
         let mut delay = Delay { };
-        for i in 0..num_step - 1 {
+        for i in 0..num_step {
             ch.mot_stp.as_ref().unwrap().set_value(1)?;
             delay.delay_us(self.step1.unwrap());
             ch.mot_stp.as_ref().unwrap().set_value(0)?;
@@ -203,17 +203,18 @@ impl SparkfunQuadStep {
         Ok(())
     }
 
-    // Really important: use this method only if you are using the BUS header of the board
+    // Really important: use this method only if you are using the BUS header of the board(you must weld the BUS enable on the read of the board)
     // With this method the Sparkfun QuadStep board can drive all four stepper using only the BUS
     // Note: at the moment, using the BUS header you cannot specify the the step size
     pub fn run_by_bus(stp_pin: u32, en_pin: u32, dir_pin: u32, num_step: i32, torque: u32) -> Result<(), gpio_cdev::Error> {
-        let stp_pin = Chip::new("/dev/gpiochip0")?
+        let mut gpio_chip = Chip::new("/dev/gpiochip0")?;
+        let stp_pin = gpio_chip
             .get_line(stp_pin)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let en_pin = Chip::new("/dev/gpiochip0")?
+        let en_pin = gpio_chip
             .get_line(en_pin)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let dir_pin = Chip::new("/dev/gpiochip0")?
+        let dir_pin = gpio_chip
             .get_line(dir_pin)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
         let dir = if num_step > 0 { 1 } else { 0 };
@@ -222,7 +223,7 @@ impl SparkfunQuadStep {
         en_pin.set_value(0)?;
         dir_pin.set_value(dir)?;
         let mut delay = Delay { };
-        for i in 0..num_step - 1 {
+        for i in 0..num_step {
             stp_pin.set_value(1)?;
             delay.delay_us(step);
             stp_pin.set_value(0)?;
