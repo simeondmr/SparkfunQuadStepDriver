de8bc7eb46d58a7b169cff648443b9ac4be57590
simeondmr <dmrsim94@gmail.com>
2021-01-31 09:04:28 +0000

    Allowed the use of a single channel

--- src/lib.rs
+++ src/lib.rs
@@ -51,48 +51,30 @@ const STEPMIN: u32 = 800;
 const STEP_MODE: ((u8,u8, u8), (u8,u8, u8), (u8,u8, u8),(u8,u8, u8),(u8,u8, u8)) = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 0), (1, 1, 1));
 
 impl SparkfunQuadStep {
-    pub fn new(mot_stp1: u32, mot_stp2: u32, mot_stp3: u32, mot_stp4: u32) -> Result<SparkfunQuadStep, gpio_cdev::Error> {
-        let mut gpio_chip = Chip::new("/dev/gpiochip0")?;
-        let mot_stp1 = gpio_chip
-            .get_line(mot_stp1)?
-            .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp2 = gpio_chip
-            .get_line(mot_stp2)?
-            .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp3 = gpio_chip
-            .get_line(mot_stp3)?
-            .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        let mot_stp4 = gpio_chip
-            .get_line(mot_stp4)?
-            .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
-        mot_stp1.set_value(0)?;
-        mot_stp2.set_value(0)?;
-        mot_stp3.set_value(0)?;
-        mot_stp4.set_value(0)?;
-        Ok(SparkfunQuadStep {
+    pub fn new() -> SparkfunQuadStep {
+        SparkfunQuadStep {
             ch1: Some(SparkfunQuadStepChannel {
-                mot_stp: Some(mot_stp1),
                 ..Default::default()
             }),
             ch2: Some(SparkfunQuadStepChannel {
-                mot_stp: Some(mot_stp2),
                 ..Default::default()
             }),
             ch3: Some(SparkfunQuadStepChannel {
-                mot_stp: Some(mot_stp3),
                 ..Default::default()
             }),
             ch4: Some(SparkfunQuadStepChannel {
-                mot_stp: Some(mot_stp4),
                 ..Default::default()
             }),
             ..Default::default()
-        })
+        }
     }
 
     //This method allow to add a new stepper
-    pub fn add_mot(&mut self, mot: Motor, mot_en: u32, mot_dir: u32, mot_ms1: u32, mot_ms2: u32, mot_ms3: u32) -> Result<(), gpio_cdev::Error> {
+    pub fn add_mot(&mut self, mot: Motor, mot_stp:u32, mot_en: u32, mot_dir: u32, mot_ms1: u32, mot_ms2: u32, mot_ms3: u32) -> Result<(), gpio_cdev::Error> {
         let mut gpio_chip = Chip::new("/dev/gpiochip0")?;
+        let mot_stp = gpio_chip
+            .get_line(mot_stp)?
+            .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
         let mot_en = gpio_chip
             .get_line(mot_en)?
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
@@ -110,40 +92,18 @@ impl SparkfunQuadStep {
             .request(LineRequestFlags::OUTPUT, 1, "SparkfunQuadStepDriver")?;
         mot_en.set_value(1)?;
         mot_dir.set_value(0)?;
-        match mot {
-            Motor::Ch1 => {
-                let ch1 = self.ch1.as_mut().unwrap();
-                ch1.mot_en = Some(mot_en);
-                ch1.mot_dir = Some(mot_dir);
-                ch1.mot_ms1 = Some(mot_ms1);
-                ch1.mot_ms2 = Some(mot_ms2);
-                ch1.mot_ms3 = Some(mot_ms3);
-            }
-            Motor::Ch2 => {
-                let ch2 = self.ch2.as_mut().unwrap();
-                ch2.mot_en = Some(mot_en);
-                ch2.mot_dir = Some(mot_dir);
-                ch2.mot_ms1 = Some(mot_ms1);
-                ch2.mot_ms2 = Some(mot_ms2);
-                ch2.mot_ms3 = Some(mot_ms3);
-            }
-            Motor::Ch3 => {
-                let ch3 = self.ch3.as_mut().unwrap();
-                ch3.mot_en = Some(mot_en);
-                ch3.mot_dir = Some(mot_dir);
-                ch3.mot_ms1 = Some(mot_ms1);
-                ch3.mot_ms2 = Some(mot_ms2);
-                ch3.mot_ms3 = Some(mot_ms3);
-            }
-            Motor::Ch4 => {
-                let ch4 = self.ch4.as_mut().unwrap();
-                ch4.mot_en = Some(mot_en);
-                ch4.mot_dir = Some(mot_dir);
-                ch4.mot_ms1 = Some(mot_ms1);
-                ch4.mot_ms2 = Some(mot_ms2);
-                ch4.mot_ms3 = Some(mot_ms3);
-            }
-        }
+        let ch = match mot {
+            Motor::Ch1 => self.ch1.as_mut().unwrap(),
+            Motor::Ch2 => self.ch2.as_mut().unwrap(),
+            Motor::Ch3 => self.ch3.as_mut().unwrap(),
+            Motor::Ch4 =>self.ch4.as_mut().unwrap()
+        };
+        ch.mot_stp = Some(mot_stp);
+        ch.mot_en = Some(mot_en);
+        ch.mot_dir = Some(mot_dir);
+        ch.mot_ms1 = Some(mot_ms1);
+        ch.mot_ms2 = Some(mot_ms2);
+        ch.mot_ms3 = Some(mot_ms3);
         Ok(())
     }
 
@@ -192,7 +152,7 @@ impl SparkfunQuadStep {
         ch.mot_ms3.as_ref().unwrap().set_value(ms_pin.2)?;
         ch.mot_en.as_ref().unwrap().set_value(0)?;
         let mut delay = Delay { };
-        for i in 0..num_step {
+        for _ in 0..num_step {
             ch.mot_stp.as_ref().unwrap().set_value(1)?;
             delay.delay_us(self.step1.unwrap());
             ch.mot_stp.as_ref().unwrap().set_value(0)?;
@@ -203,7 +163,7 @@ impl SparkfunQuadStep {
         Ok(())
     }
 
-    // Really important: use this method only if you are using the BUS header of the board(you must weld the BUS enable on the read of the board)
+    // Really important: use this method only if you are using the BUS header of the board(you must weld the BUS enable on the rear of the board)
     // With this method the Sparkfun QuadStep board can drive all four stepper using only the BUS
     // Note: at the moment, using the BUS header you cannot specify the the step size
     pub fn run_by_bus(stp_pin: u32, en_pin: u32, dir_pin: u32, num_step: i32, torque: u32) -> Result<(), gpio_cdev::Error> {
@@ -223,7 +183,7 @@ impl SparkfunQuadStep {
         en_pin.set_value(0)?;
         dir_pin.set_value(dir)?;
         let mut delay = Delay { };
-        for i in 0..num_step {
+        for _ in 0..num_step {
             stp_pin.set_value(1)?;
             delay.delay_us(step);
             stp_pin.set_value(0)?;
